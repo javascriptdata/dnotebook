34e6977cbc6958e87351ab75ae4d513041aef089
Rising Odegua <risingodegua@gmail.com>
2021-11-11 20:22:00 +0100

    Set up dnotebook server to use typescript

---
+++ src/server/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Well done!');
+})
+
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!');
+})
