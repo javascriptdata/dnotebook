70f1a5d397ec2defc9566486a3864c7aff3a51ee
Rising Odegua <risingodegua@gmail.com>
2022-01-19 19:49:31 +0100

    Add global state manager

---
+++ src/frontend/lib/state/reducer.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    interpreterMode: "browser",
+}
+
+const appReducer = createSlice({
+    name: "app",
+    initialState: { ...initialState },
+    reducers: {
+        setInterpreterMode: (state, action) => {
+            state.interpreterMode = action.payload;
+        }
+    }
+});
+
+export const {
+    setInterpreterMode,
+} = appReducer.actions;
+
+export default appReducer.reducer;
--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import store from '../lib/state/store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
